cb6e1dea267867828e6ac3b6a80cc0cced9e0abe
paichato <tsenane14@gmail.com>
2022-04-15 22:00:50 +0200

    added firestore to formbox

--- src/components/FormBox/index.tsx
+++ src/components/FormBox/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from "react";
+import firestore from "@react-native-firebase/firestore";
 
-import { Container } from './styles';
-import { ButtonIcon } from '../ButtonIcon';
-import { Input } from '../Input';
+import { Container } from "./styles";
+import { ButtonIcon } from "../ButtonIcon";
+import { Input } from "../Input";
 
 export function FormBox() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(0);
+
   return (
     <Container>
       <Input
         placeholder="Nome do produto"
         size="medium"
+        onChangeText={setDescription}
       />
 
       <Input
@@ -17,13 +22,10 @@ export function FormBox() {
         keyboardType="numeric"
         size="small"
         style={{ marginHorizontal: 8 }}
+        onChangeText={(value) => setQuantity(Number(value))}
       />
 
-      <ButtonIcon
-        size='large'
-        icon="add-shopping-cart"
-        onPress={() => { }}
-      />
+      <ButtonIcon size="large" icon="add-shopping-cart" onPress={() => {}} />
     </Container>
   );
 }
