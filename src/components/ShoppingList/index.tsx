734e2bb8e8941510ae5af026b39f95010b46fd0b
paichato <tsenane14@gmail.com>
2022-04-15 22:39:20 +0200

    added get files files from server

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,13 +1,24 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList } from "react-native";
-
+import firestore from "@react-native-firebase/firestore";
 import { styles } from "./styles";
 import { Product, ProductProps } from "../Product";
 
-import { shoppingListExample } from "../../utils/shopping.list.data";
-
 export function ShoppingList() {
   const [products, setProducts] = useState<ProductProps[]>([]);
+
+  useEffect(() => {
+    firestore()
+      .collection("products")
+      .get()
+      .then((res) => {
+        const data = res.docs.map((doc) => {
+          return { id: doc.id, ...doc.data() };
+        }) as ProductProps[];
+        setProducts(data);
+      })
+      .catch((error) => console.log("ERROR GETTING FILES:", error));
+  }, []);
 
   return (
     <FlatList
