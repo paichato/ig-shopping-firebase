5b6e4e8f718d0beab4d07aebabcbe99805318d84
paichato <tsenane14@gmail.com>
2022-04-16 17:52:16 +0200

    added auth to routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 
 import { AppRoutes } from "./app.routes";
 import { SignIn } from "../screens/SignIn";
+import auth from "@react-native-firebase/auth";
+
+type User = {
+  uid: string;
+};
 
 export function Routes() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((userInfo) => {
+      console.log(userInfo);
+
+      setUser(userInfo);
+    });
+
+    return subscriber;
+  }, []);
+
   return (
     <NavigationContainer>
-      <SignIn />
+      {user ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   );
 }
